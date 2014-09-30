ff52a0dda119cd694855e45117711c856e2c27dc
Tom McLaughlin <tmclaughlin@hubspot.com>
2014-09-30 18:27:29 -0400

    add $nexus_work_dir_manage to determine whether or not the module should manage the work directory.  In my infrastructure this directory is an AWS EBS mount point and our volume management code handles this directory resultung in a duplicate resource in our infrastructure..

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,19 +30,20 @@
 # Copyright 2013 Hubspot
 #
 class nexus (
-  $version            = $nexus::params::version,
-  $revision           = $nexus::params::revision,
-  $download_site      = $nexus::params::download_site,
-  $nexus_root         = $nexus::params::nexus_root,
-  $nexus_home_dir     = $nexus::params::nexus_home_dir,
-  $nexus_work_dir     = undef,
-  $nexus_user         = $nexus::params::nexus_user,
-  $nexus_group        = $nexus::params::nexus_group,
-  $nexus_host         = $nexus::params::nexus_host,
-  $nexus_port         = $nexus::params::nexus_port,
-  $nexus_work_recurse = $nexus::params::nexus_work_recurse,
-  $nexus_context      = $nexus::params::nexus_context,
-  $nexus_manage_user  = $nexus::params::nexus_manage_user,
+  $version               = $nexus::params::version,
+  $revision              = $nexus::params::revision,
+  $download_site         = $nexus::params::download_site,
+  $nexus_root            = $nexus::params::nexus_root,
+  $nexus_home_dir        = $nexus::params::nexus_home_dir,
+  $nexus_work_dir        = undef,
+  $nexus_work_dir_manage = $nexus::params::nexus_work_dir_manage,
+  $nexus_user            = $nexus::params::nexus_user,
+  $nexus_group           = $nexus::params::nexus_group,
+  $nexus_host            = $nexus::params::nexus_host,
+  $nexus_port            = $nexus::params::nexus_port,
+  $nexus_work_recurse    = $nexus::params::nexus_work_recurse,
+  $nexus_context         = $nexus::params::nexus_context,
+  $nexus_manage_user     = $nexus::params::nexus_manage_user,
 ) inherits nexus::params {
   include stdlib
 
@@ -52,7 +53,7 @@ class nexus (
   }
 
   if $nexus_work_dir != undef {
-    $real_nexus_work_dirc= $nexus_work_dir
+    $real_nexus_work_dirc = $nexus_work_dir
   } else {
     $real_nexus_work_dir = "${nexus_root}/sonatype-work/nexus"
   }
@@ -76,17 +77,18 @@ class nexus (
   }
 
   class{ 'nexus::package':
-    version            => $version,
-    revision           => $revision,
-    download_site      => $download_site,
-    nexus_root         => $nexus_root,
-    nexus_home_dir     => $nexus_home_dir,
-    nexus_user         => $nexus_user,
-    nexus_group        => $nexus_group,
-    nexus_work_dir     => $real_nexus_work_dir,
-    nexus_work_recurse => $nexus_work_recurse,
-    require            => Anchor['nexus::begin'],
-    notify             => Class['nexus::service']
+    version               => $version,
+    revision              => $revision,
+    download_site         => $download_site,
+    nexus_root            => $nexus_root,
+    nexus_home_dir        => $nexus_home_dir,
+    nexus_user            => $nexus_user,
+    nexus_group           => $nexus_group,
+    nexus_work_dir        => $real_nexus_work_dir,
+    nexus_work_dir_manage => $nexus_work_dir_manage,
+    nexus_work_recurse    => $nexus_work_recurse,
+    require               => Anchor['nexus::begin'],
+    notify                => Class['nexus::service']
   }
 
   class{ 'nexus::config':
--- manifests/package.pp
+++ manifests/package.pp
@@ -36,6 +36,7 @@ class nexus::package (
   $nexus_user,
   $nexus_group,
   $nexus_work_dir,
+  $nexus_work_dir_manage,
   $nexus_work_recurse,
 ) inherits nexus::params {
 
@@ -80,12 +81,17 @@ class nexus::package (
     require => Exec[ 'nexus-untar']
   }
 
-  file{ $nexus_work_dir:
-    ensure  => directory,
-    owner   => $nexus_user,
-    group   => $nexus_group,
-    recurse => $nexus_work_recurse,
-    require => Exec[ 'nexus-untar']
+
+  # I have an EBS volume for $nexus_work_dir and mounting code in our tree
+  # creates this and results in a duplicate resource. -tmclaughlin
+  if $nexus_work_dir_manage == true {
+    file{ $nexus_work_dir:
+      ensure  => directory,
+      owner   => $nexus_user,
+      group   => $nexus_group,
+      recurse => $nexus_work_recurse,
+      require => Exec[ 'nexus-untar']
+    }
   }
 
   file{ $nexus_home:
