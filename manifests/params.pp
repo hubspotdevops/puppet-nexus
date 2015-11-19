a4d5a4a045e0cd234edbf6c6b628fd7929599db2
Plague-doctor <github10@cryptolab.net>
2015-11-19 11:56:47 +1100

    Add nexus_selinux_ignore_defaults to prevent enforcing SELinux ownership of the files.

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,19 +21,20 @@
 #
 class nexus::params {
   # See nexus::package on why this won't increment the package version.
-  $version               = 'latest'
-  $revision              = '01'
-  $deploy_pro            = false
-  $download_site         = 'http://download.sonatype.com/nexus/oss'
-  $nexus_root            = '/srv'
-  $nexus_home_dir        = 'nexus'
-  $nexus_work_recurse    = true
-  $nexus_work_dir_manage = true
-  $nexus_user            = 'nexus'
-  $nexus_group           = 'nexus'
-  $nexus_host            = '0.0.0.0'
-  $nexus_port            = '8081'
-  $nexus_context         = '/nexus'
-  $nexus_manage_user     = true
-  $pro_download_site     = 'http://download.sonatype.com/nexus/professional-bundle'
+  $version                       = 'latest'
+  $revision                      = '01'
+  $deploy_pro                    = false
+  $download_site                 = 'http://download.sonatype.com/nexus/oss'
+  $nexus_root                    = '/srv'
+  $nexus_home_dir                = 'nexus'
+  $nexus_work_recurse            = true
+  $nexus_work_dir_manage         = true
+  $nexus_selinux_ignore_defaults = true
+  $nexus_user                    = 'nexus'
+  $nexus_group                   = 'nexus'
+  $nexus_host                    = '0.0.0.0'
+  $nexus_port                    = '8081'
+  $nexus_context                 = '/nexus'
+  $nexus_manage_user             = true
+  $pro_download_site             = 'http://download.sonatype.com/nexus/professional-bundle'
 }
